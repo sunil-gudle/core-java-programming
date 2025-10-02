41639724a7b29ced138d0c3b9cf2adf764421e47
sunil-gudle <sunilgudle7844@gmail.com>
2025-10-02 15:47:27 +0530

    First program - Unicode program

---
+++ src/main/java/com/sunil/dailyCoding/basicPrograms/UnicodeProgram.java
@@ -0,0 +1,9 @@
+package com.sunil.dailyCoding.basicPrograms;
+
+public class UnicodeProgram {
+    public static void main(String[] args) {
+        for (char c=0x0900; c<=0x0970; c++){
+            System.out.print(c + " ");
+        }
+    }
+}
